305d36326fb8544b85f85d8758d607c1051925cb
Mozzka <yoyo122213@gmail.com>
2025-03-10 19:25:50 +0700

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -2,14 +2,26 @@ function playAudio() {
     var audio = document.getElementById("myAudio");
     audio.muted = false;
     audio.play();
-  }
-  document.body.addEventListener("click", playAudio);
-  document.body.addEventListener("touchstart", playAudio);
 
-  function disableScroll() {
-        document.body.style.overflow = 'hidden';
-        }
-        function enableScroll() {
-        document.body.style.overflow = 'auto';
-        }
-        disableScroll();
+    // Mengatur Media Session API
+    if ('mediaSession' in navigator) {
+        navigator.mediaSession.metadata = new MediaMetadata({
+            title: "I Quit Drinking",
+            artist: "Kelsea Ballerini, LANY",
+            artwork: [
+                { src: "all/cover.jpg", sizes: "512x512", type: "image/jpeg" }
+            ]
+        });
+    }
+}
+
+document.body.addEventListener("click", playAudio);
+document.body.addEventListener("touchstart", playAudio);
+
+function disableScroll() {
+    document.body.style.overflow = 'hidden';
+}
+function enableScroll() {
+    document.body.style.overflow = 'auto';
+}
+disableScroll();
